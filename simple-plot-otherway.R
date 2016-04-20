6131ce74f075119647242304a94224714d06c697
Austin Davis <austinvdavis@gmail.com>
2016-04-20 09:09:13 -0500

    october plots

--- simple-plot-otherway.R
+++ simple-plot-otherway.R
@@ -1,4 +1,4 @@
-p <- "/Users/austinstig/Developer/CS512/src/january.txt";
+p <- "/Users/austinstig/Developer/CS512/src/october.txt";
 data <- read.csv(p, header=TRUE, sep = ",");
 # get day of week
 data$day <-weekdays(as.Date(strptime(data$time, "%Y-%m-%d %H:%M:%S")));
@@ -10,45 +10,51 @@ non_edu <- subset(data, edu == 0);
 # segment by hour
 b <- c()
 colors <- c()
+nam <- c()
 for (hr in seq(1,23)) {
   # subset data
-  e <- subset(edu, hour = hr);
-  n <- subset(non_edu, hour = hr);
+  e <- subset(edu, hour == hr);
+  n <- subset(non_edu, hour == hr);
   # append data
-  b<- append(b, sum(e$score)/sum(e$count));
-  b<- append(b, sum(n$score)/sum(n$count));
+  b<- append(b, mean(e$score));
+  b<- append(b, mean(n$score));
   # append color
   colors<-append(colors, "blue");
   colors<-append(colors, "red");
+  # append label
+  nam<-append(nam, hr);
+  nam<-append(nam, hr);
 }
-
 # plot bars
-barplot(b, col=colors);
+barplot(b, col=colors, names.arg =nam);
 
 
 
 # segment by hour
 b <- c()
 colors <- c()
-for (dy in c("Monday", "Tuesday","Wednesday","Thursday","Friday")) {
+nam <- c()
+for (dy in c("Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")) {
   # subset data
   e <- subset(edu, day == dy);
   n <- subset(non_edu, day == dy);
   # append data
-  b<- append(b, sum(e$score)/sum(e$count));
-  b<- append(b, sum(n$score)/sum(n$count));
+  b<- append(b, mean(e$score));
+  b<- append(b, mean(n$score));
   # append color
   colors<-append(colors, "blue");
   colors<-append(colors, "red");
+  # append label
+  nam<-append(nam, dy);
+  nam<-append(nam, dy);
 }
-
 # plot bars
-#barplot(b, col=colors);
+barplot(b, col=colors, names.arg =nam);
 
 # apply moving averages
-#mover <- function(x, win=5) { filter(x, rep(1/win, win), sides=2) }
-#plot(edu$window, mover(edu$score), col="blue", type="l");
-#lines(edu$window, mover(not_edu$score), col="red");
+mover <- function(x, win=5) { filter(x, rep(1/win, win), sides=2) }
+plot(edu$window, mover(edu$score), col="blue", type="l");
+lines(edu$window, mover(non_edu$score), col="red");
 
 
   
